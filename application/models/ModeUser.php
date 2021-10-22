30f7879bc09c9220725dca565933874810afc77e
aldihagi <rubenhagi21@gmail.com>
2021-10-23 02:29:51 +0700

    menambahkan file new file

---
+++ application/models/ModeUser.php
@@ -0,0 +1,36 @@
+<?php
+defined('BESEPATH') or exit('No direct script access allowed');
+
+class ModelUser extends CI_Model
+{
+    public function simpanData($data= null)
+    {
+        $this->db->insert('user', $data);
+    }
+
+    public function cekData($where= null)
+    {
+        return $this->db->get_where('user', $where);
+    }
+    
+    public function getUserWhere($where= null)
+    {   
+        return $this->db->get_where('user', $where);
+    }
+
+    public function cekUserAccess($where= null)
+    {
+        $this->db->select('*');
+        $this->db->from('access_menu');
+        $this->db->where($where);
+        return $this->db->get();
+    }
+
+    public function getUserlimit($where= null)
+    {
+        $this->db->select('*');
+        $this->db->from('user');
+        $this->db->limit($limit);
+        return $this->db->get();
+    }
+}
---
+++ application/models/ModelBuku.php
@@ -0,0 +1,78 @@
+<?php
+defined('BESEPATH') or exit('No direct script access allowed');
+
+class ModelBuku extends CI_Model
+{
+    //Manajemen buku
+    public function getBuku()
+    {
+        return $this->db->get('buku');
+    }
+
+    public function bukuWhere($where)
+    {
+        return $this->db->get_where('buku', $where);
+    }
+
+    public function simpanBuku($data = null)
+    {
+        $this->db->insert('buku', $data);
+    }
+
+    public function updateBuku($data = null, $where = null)
+    {
+        $this->db->update('buku', $data, $where);
+    }
+
+    public function HapusBuku($where = null)
+    {
+        $this->db->delete('buku', $where);
+    }
+
+    public function total($field, $where)
+    {
+        $this->db->select_sum($field);
+        if(!empty($where) && count($where) > 0){
+            $this->db->where($where);
+        }
+        $this->db->form('buku');
+        return $this->db->get()->row($field);
+    }
+
+    //manajemen kategori
+    public function getKategori()
+    {
+        return $this->db->get->('katergori');
+    }
+
+    public function kategoriWhere($where)
+    {
+        return $this->db->get_where->('katergori', $where);
+    }
+
+    public function simpanKategori($data = null)
+    {
+        return $this->db->insert('katergori', $data);
+    }
+
+    public function hapusKategori($where = null)
+    {
+        return $this->db->delete('katergori', $where);
+    }
+
+    public function updateKategori($where = null, $data = null)
+    {
+        return $this->db->update('katergori', $data, $where);
+    }
+
+    //join
+    public function joinKategoriBuku($where)
+    {
+        $this->db->select('buku.id_kategori,kategori.kategori');
+        $this->db->form('buku');
+        $this->db->join('kategori', 'kategori.id buku.id_kategori');
+        $this->db->where($where);
+        return $this->db->get();
+    }
+
+}
